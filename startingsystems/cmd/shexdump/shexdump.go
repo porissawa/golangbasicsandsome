403329fdf413cadf0ac2ccae33469b11677613f1
Pedro Morissawa <pmorissawa@gmail.com>
2026-06-05 15:38:32 -0300

    shexdump: offset, acii, columns, columnWidth

--- startingsystems/cmd/shexdump/shexdump.go
+++ startingsystems/cmd/shexdump/shexdump.go
@@ -10,62 +10,122 @@
 //	//	69 64 20 6f 66 20 74 68  65 69 72 20 63 6f 75 6e
 //	//	74 72 79 20 0a
 
+// Usage: shexdump [-offset] [-columns] [-column-width] [-squeeze] [-ascii] [filename]
+
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
+type Options struct {
+	columns, columnWidth          int
+	offset, squeeze, ascii, canon bool
+}
+
 func main() {
+	var opts Options
+
+	{
+		//// read flags + assign default vals
+		flag.BoolVar(&opts.offset, "offset", false, "show offset in start of line")
+		flag.BoolVar(&opts.squeeze, "squeeze", false, "collapse equal lines into existing one, add a * for each")
+		flag.BoolVar(&opts.ascii, "ascii", false, "show ascii representation of hex as a line suffix")
+		flag.BoolVar(&opts.canon, "canon", false, "idk")
+		flag.IntVar(&opts.columns, "columns", 2, "how many columns should be shown")
+		flag.IntVar(&opts.columnWidth, "columnWidth", 8, "how wide each column is")
+		flag.Parse()
+	}
+
 	// 1. choose the input source: stdin or a file
 	var src io.Reader
-	switch len(os.Args) {
-	case 1:
+	switch flag.NArg() {
+	case 0:
 		src = os.Stdin
-	case 2:
-		f, err := os.Open(os.Args[1])
+	default:
+		f, err := os.Open(flag.Arg(0))
 		if err != nil {
-			fatalf("open %s: %v", os.Args[1], err)
+			fatalf("error: %v\n", err)
 		}
 		defer f.Close()
 		src = f
-	default:
-		fatalf("Usage: %s [filename]", os.Args[0])
 	}
-	if err := hexdump(os.Stdout, src); err != nil {
-		fatalf("hexdump: %v", err)
+
+	if err := hexdump(os.Stdout, src, opts); err != nil {
+		fatalf("hexdump: %v\n", err)
 	}
 }
 
 // dumps the contents of r to w in a hexdump format
-func hexdump(dst io.Writer, src io.Reader) error {
+func hexdump(dst io.Writer, src io.Reader, opts Options) error {
 	// performance: small reads and writes are very inefficient. While we could write a byte at a time
 	// it's much faster to read and write in chunks.
 	r := bufio.NewReader(src)
+
+	currOffset := 0
+	currHexOffset := ""
+	//// how many should I add to the buffer will be decided by these flags
+	var offset, squeeze, ascii int
+	const hex = "0123456789abcdef"
+	// default: 2 * 8 = 16
+	hexCharPerLine := opts.columns * opts.columnWidth
+
+	if opts.ascii {
+		ascii = 16 + 4
+	}
+
 	for {
-		var raw [16]byte                       // read 16 bytes at a time
-		encoded := make([]byte, 0, 16*(3+1)+1) // 16 bytes, 3 characters per byte, 1 space between bytes, newline at the end
+		raw := make([]byte, hexCharPerLine) // read buffer
+
+		if opts.offset {
+			currHexOffset = fmt.Sprintf("%.8x", currOffset)
+			// "0x" + hex + " | "
+			offset = 2 + len(currHexOffset) + 3
+		}
+
+		//// just so the compiler doesn't say anything about these variables while waiting for implementation
+		fmt.Fprintf(io.Discard, "%d", squeeze)
+
+		encoded := make([]byte, 0, hexCharPerLine*3+1+offset+ascii) // colums * columnWidth bytes, 3 characters per byte, 1 space between bytes, newline at the end
+
 		n, err := io.ReadFull(r, raw[:])
-		// convert each byte in the chunk to a pair of hexadecimal digits
-		const hex = "0123456789abcdef"
 		if n != 0 {
-			for i := range min(n, 8) {
-				//// raw[i]>>4 gets the top 4 bits of the byte (high nibble), by right-shifting by 4 bits (so the lower four are replaced)
-				//// raw[i]&0x0f gets the lower 4 bits (the low nibble), as the binary representation for the mask is 00001111
-				//// they each end up with a number from 0 to 15 ([0:(2^4 - 1)], since we're working with 4 bits of information)
-				//// which is then used as an index to lookup the value in the `hex` array
-				encoded = append(encoded, hex[raw[i]>>4], hex[raw[i]&0x0f], ' ')
+			if opts.offset {
+				encoded = append(encoded, '0', 'x')
+				for _, v := range currHexOffset {
+					encoded = append(encoded, byte(v))
+				}
+				encoded = append(encoded, ' ', '|', ' ')
 			}
-			encoded = append(encoded, ' ')
-			for i := 8; i < min(n, 16); i++ {
-				encoded = append(encoded, hex[raw[i]>>4], hex[raw[i]&0x0f], ' ')
+
+			for i := range opts.columns {
+				lastIdx := opts.columnWidth * (i + 1)
+				initIdx := opts.columnWidth * i
+				for j := initIdx; j < min(lastIdx, hexCharPerLine); j++ {
+					//// raw[i]>>4 gets the top 4 bits of the byte (high nibble), by right-shifting by 4 bits (so the lower four are replaced)
+					//// raw[i]&0x0f gets the lower 4 bits (the low nibble), as the binary representation for the mask is 00001111
+					//// they each end up with a number from 0 to 15 ([0:(2^4 - 1)], since we're working with 4 bits of information)
+					//// which is then used as an index to lookup the value in the `hex` array
+					encoded = append(encoded, hex[raw[j]>>4], hex[raw[j]&0x0f], ' ')
+				}
+				encoded = append(encoded, ' ')
+			}
+
+			if opts.ascii {
+				encoded = append(encoded, '|', ' ')
+				for _, v := range raw {
+					encoded = append(encoded, v)
+				}
+				encoded = append(encoded, ' ', '|')
 			}
 			//// add the newline to end of 16 bits line
 			encoded[len(encoded)-1] = '\n'
 		}
+		currOffset += hexCharPerLine
 		if _, err := dst.Write(encoded); err != nil {
 			return err
 		}
